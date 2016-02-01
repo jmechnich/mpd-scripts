28f0f2ab89515ecdb60f1810d1a45c959e91c96c
jmechnich <joerg.mechnich@gmail.com>
2016-02-01 21:34:36 +0100

    added distutils script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mpd-scripts',
+      version='1.0',
+      description='',
+      author='Joerg Mechnich',
+      author_email='joerg.mechnich@gmail.com',
+      url='https://github.com/jmechnich/mpd-scripts',
+      packages=['mpd_scripts'],
+      scripts=['mpd_dynamic', 'mp3gain']
+     )
