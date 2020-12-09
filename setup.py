63d7db40f0cbe801597dae20ecdada0af30723ee
jmechnich <joerg.mechnich@gmail.com>
2020-12-09 10:34:51 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
-import sys
-if sys.version_info < (3,4):
-    print("Requires python 3.4 or later")
-    sys.exit(1)
+from setuptools import find_packages, setup
 
-from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='mpd-scripts',
-    version='1.0',
-    description='',
-    url='https://github.com/jmechnich/mpd-scripts',
     author='Joerg Mechnich',
     author_email='joerg.mechnich@gmail.com',
+    description='A collection of scripts related to the Music Player Daemon.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jmechnich/mpd-scripts',
     license='MIT',
     scripts=['mpd_dynamic', 'mpd_watch', 'mp3gain'],
-    data_files=[('/etc/systemd/system',
-                 ['mpd_dynamic.service', 'mpd_watch.service'])],
-#    install_requires=["eyed3", "mpd"],
+    use_scm_version={"local_scheme": "no-local-version"},
+    setup_requires=['setuptools_scm'],
+    install_requires=["python-mpd2", "eyeD3"],
+    python_requires='>=3.6',
 )
